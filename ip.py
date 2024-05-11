0c88afec326c2ea84c6cb31aa26485ec0ec498ec
Sam <138595608+AIWizard47@users.noreply.github.com>
2024-05-11 11:25:34 +0530

    Add files via upload

--- ip.py
+++ ip.py
@@ -5,26 +5,32 @@ def get_ip_address():
     # Get the hostname of the local machine
     hostname = socket.gethostname()
     # Get the IP address corresponding to the hostname
-
     ip_address = socket.gethostbyname(hostname)
     return [ip_address , hostname]
 
+def get_device_name(ip_address):
+    try:
+        # Perform reverse DNS lookup to get the device name
+        device_name = socket.gethostbyaddr(ip_address)[0]
+        return device_name
+    except Exception as e:
+        return "Unknown"
 
 def get_connected_devices():
     try:
         # Run the arp command to get the list of connected devices
         result = subprocess.run(['arp', '-a'], capture_output=True, text=True)
         output_lines = result.stdout.split('\n')
-        
+
         # Extract the IP addresses from the output
-        ip_addresses = []
+        devices_info = []
         for line in output_lines:
             parts = line.split()
             if len(parts) >= 2:
                 ip_address = parts[0]
-                ip_addresses.append(ip_address)
-        
-        return ip_addresses
+                device_name = get_device_name(ip_address)
+                devices_info.append((ip_address, device_name))
+        return devices_info
     except Exception as e:
         print("Error:", e)
         return []
@@ -33,5 +39,5 @@ if __name__ == "__main__":
     print("Your IP Address is:", get_ip_address())
     connected_devices = get_connected_devices()
     print("Connected devices:")
-    for ip in connected_devices:
-        print(ip)
+    for ip, name in connected_devices:
+        print(f"IP Address: {ip}, Device Name: {name}")
